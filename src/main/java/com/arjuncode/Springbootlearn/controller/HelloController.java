1fa3d6baea4b5c7465f78304bfe01a72948aa90a
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2023-12-10 19:33:36 +0530

    1st Api created and server port changed

---
+++ src/main/java/com/arjuncode/Springbootlearn/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.arjuncode.Springbootlearn.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Component
+//@Controller // this @Controller annotation internally calls @Component annotation so we can remove @Component
+@RestController // we need this to be rest controller so it will always return a response body
+/* it will do 2 things
+1. it will by default a controller
+2. It will always return response body
+*/
+public class HelloController {
+
+    // "8080/" will be execute this
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String helloWorld(){
+        return "WELCOME TO LEARN SPRING BOOT ARJUN!!!";
+    }
+}
