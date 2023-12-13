6f13986dd2b1d1ca87a93e98b0396126f453a04e
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2023-12-13 19:34:21 +0530

    FETCHING DATA FROM DB WITH GET API

--- src/main/java/com/arjuncode/Springbootlearn/controller/DepartmentController.java
+++ src/main/java/com/arjuncode/Springbootlearn/controller/DepartmentController.java
@@ -4,9 +4,12 @@ import com.arjuncode.Springbootlearn.entity.Department;
 import com.arjuncode.Springbootlearn.service.DepartmentService;
 import com.arjuncode.Springbootlearn.service.DepartmentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -19,5 +22,11 @@ public class DepartmentController {
         return departmentService.saveDepartment(department);
         //DepartmentService service = new DepartmentServiceImpl();
         // this is a manual way of creating object using new keyword. but we can doi it easily with Spring-boot
+    }
+
+    //This is a get method
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList(){
+      return departmentService.fetchDepartmentList();
     }
 }
--- src/main/java/com/arjuncode/Springbootlearn/service/DepartmentService.java
+++ src/main/java/com/arjuncode/Springbootlearn/service/DepartmentService.java
@@ -2,6 +2,12 @@ package com.arjuncode.Springbootlearn.service;
 
 import com.arjuncode.Springbootlearn.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
+
+
     public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
 }
